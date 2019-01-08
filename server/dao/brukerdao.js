066d723f8419b23f1bebac2334b248e71a4f5617
Stian Ådnanes <stianaad@stud.ntnu.no>
2019-01-08 15:58:12 +0100

    fikset hashing til database

--- server/dao/brukerdao.js
+++ server/dao/brukerdao.js
@@ -1,11 +1,11 @@
 const Dao = require("./dao.js");
 
 module.exports = class brukerdao extends Dao {
-
-    lagNyBruker(sakNrStart, callback){
+    lagNyBruker(json, callback){
+        const tabell = [json.epost,json.passord,json.kommune_id];
         super.query(
-            "INSERT INTO bruker VALUES()", 
-            [],
+            "INSERT INTO bruker VALUES(DEFAULT,?,?,?)", 
+            tabell,
             callback
         );
     }
