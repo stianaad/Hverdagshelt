780b36c57b39fef1ee30e58eb08fefa08c98b1a3
sarahjel <sarahjel@stud.ntnu.no>
2019-01-09 15:03:45 +0100

    Endringer på registreringsside

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,11 @@
 /* eslint eqeqeq: "off" */
-
-
 import * as React from 'react';
 import { Component,sharedComponentData } from 'react-simplified';
 import { BrowserRouter, Route, NavLink, Redirect,Switch } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import Popup from 'reactjs-popup';
 import { sakService} from './services';
+import {Registrering} from './Komponenter/Registrering/registrering';
 
 import createBrowserHistory from 'history/createBrowserHistory';
 const history = createBrowserHistory(); // Use history.push(...) to programmatically change path, for instance after successfully saving a student
@@ -18,7 +17,7 @@ class Menu extends Component {
     return(
       <div>
         <p>hehehehe</p>
-      
+        <Registrering></Registrering>
       </div>
     )
   }
@@ -29,6 +28,7 @@ if (root)
     <BrowserRouter>
       <div>
           <Route path="/nyheter" component={Menu} />
+          <Route path="/registrering" component={Registrering}/>
       </div>
     </BrowserRouter>,
     root
