cf6418d177d7918bed5189c29fd12a1d34c50426
William Andersson <williaan@stud.ntnu.no>
2019-01-09 15:05:54 +0100

    mere dao

--- server/dao/feildao.js
+++ server/dao/feildao.js
@@ -36,6 +36,7 @@ module.exports = class FeilDao extends Dao {
   oppdaterFeil(json, callback) {
     var feil = [
       json.kategori_id,
+      json.status_id,
       json.beskrivelse,
       json.bilde,
       json.lengdegrad,
@@ -43,20 +44,20 @@ module.exports = class FeilDao extends Dao {
       json.feil_id,
     ];
     super.query(
-      'UPDATE feil SET kategori_id = ?, SET beskrivelse = ?, SET bilde = ?, SET lengdegrad = ?, SET breddegrad = ? WHERE feil_id = ?',
+      'UPDATE feil SET kategori_id = ?, SET status_id = ?, SET beskrivelse = ?, SET bilde = ?, SET lengdegrad = ?, SET breddegrad = ? WHERE feil_id = ?',
       feil,
       callback
     );
   }
 
-  endreStatusFeil(json, callback) {
+  /*endreStatusFeil(json, callback) {
     var idArray = [json.status_id, json.feil_id];
     super.query(
       'UPDATE feil SET status_id = ? WHERE feil_id = ?',
       idArray,
       callback
     );
-  }
+  }*/
 
   slettFeil(json, callback) {
     var id = json.feil_id;
@@ -70,6 +71,51 @@ module.exports = class FeilDao extends Dao {
 
   hentAlleKategorier(callback) {
     super.query('SELECT * FROM kategori', null, callback);
+  }
+
+  lagOppdatering(json, callback) {
+    var oppdatering = [
+      json.feil_id,
+      json.kommentar,
+      json.status_id,
+      json.bruker_id,
+    ];
+    super.query(
+      'INSERT INTO oppdatering (feil_id, kommentar, status_id, bruker_id) VALUES (?, ?, ?, ?)',
+      oppdatering,
+      callback
+    );
+  }
+
+  hentAlleOppdateringerPaaFeil(json, callback) {
+    var feil_id = json.feil_id;
+    super.query(
+      'SELECT * WHERE oppdatering WHERE feil_id = ?',
+      [feil_id],
+      callback
+    );
+  }
+
+  /*slettOppdatering(json, callback) {
+    var oppdatering_id = json.oppdatering_id;
+    super.query(
+      'DELETE FROM oppdatering WHERE oppdatering_id = ?',
+      [oppdatering_id],
+      callback
+    );
+  }*/
+
+  hentEnStatus(json, callback) {
+    var status_id = json.status_id;
+    super.query(
+      'SELECT * FROM status WHERE status_id = ?',
+      [status_id],
+      callback
+    );
+  }
+
+  hentAlleStatuser(callback) {
+    super.query('SELECT * FROM status', null, callback);
   }
 };
 
