0ba74bf76b4f4f1be8172d3e42f5492f9430b9e0
runevedo <runevedo@stud.ntnu.no>
2019-01-09 14:06:02 +0100

    Laget endepunkt /sjekkpassord og /endre passord med tilhørende sql-spørringer

--- server/dao/brukerdao.js
+++ server/dao/brukerdao.js
@@ -5,4 +5,15 @@ module.exports = class BrukerDao extends Dao {
     const tabell = [json.epost, json.passord, json.kommune_id];
     super.query('INSERT INTO bruker VALUES(DEFAULT,?,?,?)', tabell, callback);
   }
+
+  hentBruker(json,callback){
+      const epost = [json.epost];
+      console.log(epost);
+      super.query("SELECT passord FROM bruker WHERE epost=?",epost,callback);
+  }
+
+  endrePassord(json,callback){
+      const tabell = [json.nyttPassord, json.epost];
+      super.query("UPDATE bruker SET passord=? WHERE epost=?",tabell,callback);
+  }
 };
