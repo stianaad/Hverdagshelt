82a8b61249119184cfd673dd809b410ac65ea81a
Bjørnar Østtveit <bjornost@stud.ntnu.no>
2019-01-10 14:48:58 +0100

    kommunevelgermodul

---
+++ client/src/Moduler/KommuneVelger/kommuneVelger.js
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import { Component } from 'react-simplified';
+import { generellServices } from '../../generellServices';
+
+export class KommuneVelger extends Component {
+    sok = "";
+    listesyn = false;
+    kommuner = [];
+
+    render() {
+        return(
+            <div>
+                <input value={this.sok} onChange={this.oppdaterSok} type="text"></input>
+                <ul>
+                    <li>lol</li>
+                    <li>lol</li>
+                    <li>lol</li>
+                    <li>lol</li>
+                    <li>lol</li>
+                </ul>
+            </div>
+        );
+    }
+
+    mounted() {
+        generellServices.hentAlleKommuner().then((res) => {
+            console.log(res);
+        });
+    }
+
+    oppdaterSok(e) {
+        this.sok = e.target.value;
+
+        if (this.sok.length > 0) {
+            this.listesyn = true;
+        } else {
+            this.listesyn = false;
+        }
+    }
+}
