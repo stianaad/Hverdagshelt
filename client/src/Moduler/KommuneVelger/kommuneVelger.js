9a9e1afd77c7bc79117e491ae3ba37fac6a50827
Bjørnar Østtveit <bjornost@stud.ntnu.no>
2019-01-10 15:49:31 +0100

    kommunevelger som fungerer

--- client/src/Moduler/KommuneVelger/kommuneVelger.js
+++ client/src/Moduler/KommuneVelger/kommuneVelger.js
@@ -6,17 +6,16 @@ export class KommuneVelger extends Component {
     sok = "";
     listesyn = false;
     kommuner = [];
+    kommuner_filtrert = [];
 
     render() {
         return(
-            <div>
-                <input value={this.sok} onChange={this.oppdaterSok} type="text"></input>
-                <ul>
-                    <li>lol</li>
-                    <li>lol</li>
-                    <li>lol</li>
-                    <li>lol</li>
-                    <li>lol</li>
+            <div className="komBoks">
+                <input className="komSok form-control" value={this.sok} onChange={this.oppdaterSok} type="text"></input>
+                <ul className="komListe" style={{display: this.listesyn ? "block" : "none"}}>
+                    {this.kommuner_filtrert.map((kommune) => (
+                        <li key={kommune.kommune_id} className="komElement"><a href={"/"+kommune.kommune_navn.toLowerCase()}>{kommune.kommune_navn}</a></li>
+                    ))}
                 </ul>
             </div>
         );
@@ -24,7 +23,7 @@ export class KommuneVelger extends Component {
 
     mounted() {
         generellServices.hentAlleKommuner().then((res) => {
-            console.log(res);
+            this.kommuner = res;
         });
     }
 
@@ -32,6 +31,19 @@ export class KommuneVelger extends Component {
         this.sok = e.target.value;
 
         if (this.sok.length > 0) {
+            this.kommuner_filtrert = [];
+            for (let i = 0; i < this.kommuner.length; i++) {
+                let match = true;
+                for (let j = 0; j < this.sok.length; j++) {
+                    if (this.kommuner[i].kommune_navn.toLowerCase().charAt(j) != this.sok.toLowerCase().charAt(j)) {
+                        match = false;
+                    }
+                }
+                if (match) this.kommuner_filtrert.push(this.kommuner[i]);
+            }
+
+
+
             this.listesyn = true;
         } else {
             this.listesyn = false;
