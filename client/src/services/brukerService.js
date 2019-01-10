1d85fb9fcde6fb1de25533c1d759feb98f1d166c
oivindhl <oivindhl@stud.ntnu.no>
2019-01-10 10:01:42 +0100

    bigboi services

--- client/src/services/brukerService.js
+++ client/src/services/brukerService.js
@@ -7,12 +7,10 @@ class BrukerService {
     return axios.post('/api/lagNyBruker', nyBruker);
   }
 
-
-
-
-
-
-
+  endrePassord(nyInformasjon) {
+    return axios.post('/api/lagNyBruker', nyInformasjon);
+  }
+  
 }
 
 export let brukerService = new BrukerService();
--- client/src/services/feilService.js
+++ client/src/services/feilService.js
@@ -1,2 +1,76 @@
 import axios from 'axios';
 axios.interceptors.response.use((response) => response.data);
+
+export class Feil {
+  feil_id;
+  kommune_id;
+  kategori_id;
+  status_id;
+  beskrivelse;
+  bilde;
+  lengdegrad;
+  breddegrad;
+}
+
+class FeilService {
+
+  hentAlleFeil() {
+    axios.get('/api/hentAlleFeil');
+  }
+
+  hentEnFeil(feil_id) {
+    axios.get('/api/hentEnFeil', feil_id);
+  }
+
+  hentFeilFraStatus(status_id) {
+    axios.get('/api/hentFeilStatus', status_id);
+  }
+
+  lagNyFeil(nyFeil) {
+    axios.post('/api/lagNyFeil', nyFeil);
+  }
+
+  oppdaterFeil(oppdatertFeil) {
+    axios.post('/api/oppdaterFeil', oppdatertFeil);
+  }
+
+  settNyStatusTilFeil(input) {
+    axios.post('/api/endreStatusFeil', input);
+  }
+
+  slettFeil(feil_id) {
+    axios.post('/api/slettFeil', feil_id);
+  }
+
+  hentEnKategori(kat_id) {
+    axios.get('/api/hentEnKategori', kat_id);
+  }
+
+  hentAlleKategorier() {
+    axios.get('/api/hentAlleKategorier');
+  }
+
+  nyOppdatering(nyOpp) {
+    axios.post('/api/lagOppdatering', nyOpp);
+  }
+
+  hentAlleOppdateringerPaaFeil(feil_id) {
+    axios.get('/api/hentAlleOppdateringerPaaFeil', feil_id);
+  }
+
+  hentEnStatus(status_id) {
+    axios.get('/api/hentEnStatus', status_id);
+  }
+
+  hentAlleStatuser() {
+    axios.get('/api/hentAlleStatuser');
+  }
+
+  hentAlleHovedkategorier() {
+    axios.get('/api/hentAlleHovedkategorier');
+  }
+
+  hentAlleSubkategorierPaaHovedkategori(hk_id) {
+    axios.get('/api/hentAlleSubKategorierPaaHovedkategori', hk_id)
+  }
+}
