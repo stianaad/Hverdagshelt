f8745c58d85f0204d7320d509c3fb211f77b9b22
Bjørnar Østtveit <bjornost@stud.ntnu.no>
2019-01-14 13:01:25 +0100

    login funker nesten

--- client/src/Moduler/login/login.js
+++ client/src/Moduler/login/login.js
@@ -1,26 +1,43 @@
 //kjør boi
 
 import * as React from 'react';
-import {Component, sharedComponentData} from 'react-simplified';
+import { Component, sharedComponentData } from 'react-simplified';
+import {brukerService} from '../../services/brukerService';
 
 export class Login extends Component {
 
+  data = {
+    epost: "",
+    passord: "",
+  }
+
   render() {
-    return ( 
+    return (
       <div className="dropdown">
         <button type="button" className="btn btn-default navbar-btn" data-toggle="dropdown">
           Login
         </button>
         <div className="dropdown-menu dropdown-menu-right dropdownbox">
-            <div className="form-group">
-              <label htmlFor="epost">E-post:</label>
-              <input type="text" className="form-control" id="epost" placeholder="E-post"></input>
-              <label htmlFor="passord">Passord:</label>
-              <input type="password" className="form-control" id="passord" placeholder="Passord"></input>
-            </div>
-            <button className="btn btn-default">Logg inn</button>
+          <div className="form-group">
+            <label htmlFor="epost">E-post:</label>
+            <input type="text" value={this.data.epost} onChange={this.endre} name="epost" className="form-control" id="epost" placeholder="E-post"></input>
+            <label htmlFor="passord">Passord:</label>
+            <input type="password" value={this.data.passord} onChange={this.endre} name ="passord" className="form-control" id="passord" placeholder="Passord"></input>
+          </div>
+          <button className="btn btn-default" onClick={this.login}>Logg inn</button>
         </div>
       </div>
     );
   }
+
+  endre(e) {
+    this.data[e.target.name] = e.target.value;
+  }
+
+  login() {
+    brukerService.loggInn(this.data).then((res) => {
+      alert(res);
+    });
+  }
+
 }
