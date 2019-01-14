edf121f56bafbe20d44b8d8e4c9962f6b7e1f6ab
Bjørnar Østtveit <bjornost@stud.ntnu.no>
2019-01-14 09:56:16 +0100

    passord

--- server/test/poolsetup.js
+++ server/test/poolsetup.js
@@ -4,7 +4,7 @@ export var pool = mysql.createPool({
     connectionLimit: 5,
     host: 'mysql.stud.iie.ntnu.no',
     user: 'bjornost',
-    password: 'bjornost',
+    password: 'lOQEZcZM',
     database: 'bjornost',
     debug: false,
     multipleStatements: true,
