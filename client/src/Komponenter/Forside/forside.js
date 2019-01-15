f090504cc553c0376c7acbcd8f726ba3c974175f
Bjørnar Østtveit <bjornost@stud.ntnu.no>
2019-01-15 11:24:30 +0100

    framside

---
+++ client/src/Komponenter/Forside/forside.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { Login } from '../../Moduler/login/login'
+import { KommuneVelger } from '../../Moduler/KommuneVelger/kommuneVelger';
+
+export class Forside extends Component {
+
+  render() {
+    return (
+      <div>
+        <header className="main-header">
+          <img
+            src="FremsideHelt.png"
+            alt="Hverdagshelt logo"
+          />
+          <div className="float-right">
+            <button
+              type="button"
+              className="main-header-button btn btn-danger border border-dark mr-4"
+            >
+              Meld inn feil
+          </button>
+            <button
+              type="button"
+              className="main-header-button btn btn-light border border-dark mr-4"
+            >
+              Hendelser
+          </button>
+          <div className="main-header-buttons d-inline-block"> <Login /></div>
+          </div>
+        </header>
+        <div className="main-center">
+          <div className="main-content">
+            <h6 className="main-title">Kommuniser direkte med din kommune </h6>
+            <KommuneVelger history={this.props.history} />
+          </div>
+        </div>
+        <footer>
+          <div className="m-5" />
+        </footer>
+      </div>
+    );
+  }
+}
