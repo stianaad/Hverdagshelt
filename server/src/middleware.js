3385abec8359b86481551e3f69cb09dcacea8b6a
oivindhl <oivindhl@stud.ntnu.no>
2019-01-16 15:48:35 +0100

    ;(

--- server/src/middleware.js
+++ server/src/middleware.js
@@ -1,5 +1,9 @@
 let jwt = require('jsonwebtoken');
 const config = require('./config.json');
+import BrukerDao from './dao/brukerdao';
+import {pool} from '../test/poolsetup';
+
+let brukerdao = new BrukerDao(pool);
 
 export let checkToken = (req, res, next) => {
   let token = req.headers['x-access-token'] || req.headers['authorization']; // Express headers are auto converted to lowercase
@@ -28,4 +32,30 @@ export let checkToken = (req, res, next) => {
   }
 };
 
-export let finnBrukerId
+export let createToken = (req, res, next) => {
+  console.log('Inne i createToken')
+  let elele = { epost: req.body.epost };
+  brukerdao.hentBruker(elele, (status, info) => {
+    console.log(info)
+    let aa = { bruker_id: info[0].bruker_id };
+    let user = {user: info};
+    console.log(aa);
+    brukerdao.hentBrukerRolle(aa, (status, data) =>{
+      console.log(data);
+      console.log(data[3].privatbruker);
+      let rolle = { role: ''};
+
+      if      (parseInt(data[3].privatbruker) == 1)  { rolle.role = 'privatbruker'; }
+      else if (parseInt(data[1].ansatt) == 1)        { rolle.role = 'ansatt'; }
+      else if (parseInt(data[2].bedrift) == 1)       { rolle.role = 'bedrift'; }
+      else                              { rolle.role = 'admin'; }
+
+      jwt.sign({user: user.user, role: rolle.role}, secret.secret, { expiresIn: '1m' }, (err, token) => {
+        console.log(err);
+        res.json({
+          token: token
+        });
+      });
+    });
+  });
+};
