9af3197378e436524bcdc68a101a95191d7a348a
sarahjel <sarahjel@stud.ntnu.no>
2019-01-17 09:13:50 +0100

    lagt til visning av feil på min side

--- client/src/Komponenter/MinSide/minside.js
+++ client/src/Komponenter/MinSide/minside.js
@@ -10,20 +10,125 @@ import {
     Icon,
     Image,
     Modal,
+    Input,
+    List
   } from 'semantic-ui-react';
   import {FeedEvent, FeedHendelse, Filtrer, Info, FeedMinside} from '../../Moduler/cardfeed';
 import { brukerService } from '../../services/brukerService';
+import {NavLink} from 'react-router-dom';
+import {markerTabell,ShowMarkerMap } from '../../Moduler/kart/map';
+
 
 export class Minside extends Component {
     rapporterteFeil = [];
     folgteFeil = [];
     folgteHendelser = [];
+    valgtFeil = {
+        overskrift: "",
+        bilde: "",
+        beskrivelse: ""
+    }
 
+    state = {open: false}
+
+    handleOpen = (feil) => {
+        this.valgtFeil = {...feil};
+        this.setState({ open: true });
+    }
+
+    handleClose = () => {this.setState({open: false})}
 
     render(){
         return(
             <div>
                 <PageHeader history={this.props.history}/>
+                <Modal open={this.state.open} onClose={this.handleClose} size="small" centered={true} dimmer="blurring">
+                    {/*<Modal.Header>
+                        {this.valgtFeil.overskrift}
+                    </Modal.Header>*/}
+                    <Modal.Content>
+                        <div>
+                            <Card fluid="true">
+                                <Card.Content>
+                                    <div>
+                                        <h1>
+                                            {this.valgtFeil.overskrift}
+                                            <NavLink
+                                                to={'/minside/11'}
+                                                onClick={this.handleClose}
+                                            >
+                                                <img
+                                                className="float-right"
+                                                src="https://image.freepik.com/free-icon/x_318-27992.jpg"
+                                                width="20"
+                                                height="20"
+                                                />
+                                            </NavLink>
+                                        </h1>
+                                        <h6>Status: {this.valgtFeil.status}{' '}
+                                            <img src='/warningicon.png' width="30" height="30" />
+                                        </h6>
+                                    </div>
+                                </Card.Content>
+                                <Card.Content extra>
+                                    <Grid fluid columns={3}>
+                                        <Grid.Column>
+                                            <h6>Beskrivelse</h6>
+                                            <Input>{this.valgtFeil.beskrivelse}</Input>
+
+                                        </Grid.Column>
+                                        <Grid.Column>
+                                            <h6>Posisjon</h6>
+                                            <ShowMarkerMap
+                                                width="100%"
+                                                height="300px"
+                                                id="posmap"
+                                                feil={this.valgtFeil}
+                                            />
+                                        </Grid.Column>
+                                        <Grid.Column>
+                                        <List>
+                                            <List.Item>
+                                                <List.Content>
+                                                    <List.Header>Godkjent</List.Header>
+                                                    <List.Description>01.01.18 19:00</List.Description>
+                                                </List.Content>
+                                            </List.Item>
+                                            <List.Item>
+                                                <List.Content>
+                                                    <List.Header>Godkjent</List.Header>
+                                                    <List.Description>01.01.18 19:00</List.Description>
+                                                </List.Content>
+                                            </List.Item>
+                                            <List.Item>
+                                                <List.Content>
+                                                    <List.Header>Godkjent</List.Header>
+                                                    <List.Description>01.01.18 19:00</List.Description>
+                                                </List.Content>
+                                            </List.Item>
+                                            <List.Item>
+                                                <List.Content>
+                                                    <List.Header>Godkjent</List.Header>
+                                                    <List.Description>01.01.18 19:00</List.Description>
+                                                </List.Content>
+                                            </List.Item>
+                                        </List>
+                                        <Image.Group size='tiny'>
+                                            <Image src="/lofoten.jpg" />
+                                            <Image src="/lofoten.jpg" />
+                                            <Image src="/lofoten.jpg" />
+                                            <Image src="/lofoten.jpg" />
+                                            <Image src="/lofoten.jpg" />
+                                            <Image src="/lofoten.jpg" />
+                                        </Image.Group>
+                                        </Grid.Column>
+                                    </Grid>
+                                </Card.Content>
+                            </Card>
+                        </div>
+                    </Modal.Content>
+                </Modal>
+
                 <h1 className="text-center">Min side</h1>
                 <div className="row ">
                     <div className="col mt-3 ml-3">
@@ -59,6 +164,7 @@ export class Minside extends Component {
                             tid={feil.tid}
                             kategori={feil.kategorinavn}
                             fjern={() => this.fjernFeil()}
+                            onClick={() => this.handleOpen(feil)}
                           >
                             {feil.overskrift}
                           </FeedMinside>
@@ -71,8 +177,8 @@ export class Minside extends Component {
                     <h2>Hendelser du følger</h2>
                     <Card.Group itemsPerRow={1}>
                         {this.folgteHendelser.map(hendelse => (
-                            <Card>
-                            <Image src={hendelse.bilde} className="feilCard"/>
+                            <Card >
+                            <Image src={hendelse.bilde}/>
                             <Card.Content>
                                 <Card.Header>{hendelse.overskrift}</Card.Header>
                                 <Card.Description><img className="mr-2"
@@ -88,7 +194,7 @@ export class Minside extends Component {
                         <h2>Feil/mangler du følger</h2>
                         <Card.Group itemsPerRow={1}>
                         {this.folgteFeil.map(feil => (
-                            <Card >
+                            <Card>
                                 <Image src={feil.url} className="feilCard"/>
                                 <Card.Content>
                                     <Card.Header>{feil.overskrift}</Card.Header>
@@ -101,16 +207,13 @@ export class Minside extends Component {
                         ))}
                     </Card.Group>
                     </div>
-                    <div className="col">
-
-                    </div>
                 </div>
             </div>
         )
     }
 
     fjernFeil(){
-        
+        console.log("fjern");
     }
 
     async mounted(){
