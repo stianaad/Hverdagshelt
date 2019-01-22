4478bf21951532bb209b731a030ad10d6ed2a344
Bjørnar Østtveit <bjornost@stud.ntnu.no>
2019-01-22 15:04:17 +0100

    fiksa kart med ssl

--- client/src/Moduler/kart/map.js
+++ client/src/Moduler/kart/map.js
@@ -336,7 +336,21 @@ export class PositionMap extends Component {
   marker = null;
 
   locateMe() {
-    this.map.locate({setView: true, maxZoom: 14});
+    navigator.geolocation.getCurrentPosition((pos) => {
+      //alert(pos.coords.latitude+":"+pos.coords.longitude);
+      if (this.marker == undefined) {
+        this.marker = L.marker({ lat: pos.coords.latitude, lng: pos.coords.longitude }, {
+          draggable: !L.Browser.mobile,
+        }).addTo(this.map);
+      } else {
+        this.marker.setLatLng({ lat: pos.coords.latitude, lng: pos.coords.longitude });
+      }
+      this.map.setView({ lat: pos.coords.latitude, lng: pos.coords.longitude }, 14)
+    }, (err) => {
+      //alert(err.code);
+    });
+    
+    /*this.map.locate({setView: true, maxZoom: 14});
     this.map.on('locationfound', () => {
       if (this.marker == undefined) {
         this.marker = L.marker(this.map.getCenter(), {
@@ -346,7 +360,7 @@ export class PositionMap extends Component {
       else {
         this.marker.setLatLng(this.map.getCenter());
       }
-    });
+    });*/
   }
 
   clicked(e) {
@@ -397,10 +411,10 @@ export class PositionMap extends Component {
         <button
           style={{position: 'absolute', top: '10px', right: '10px', zIndex: '900', height: '35px', cursor: 'pointer'}}
           id="locatebtn"
-          type="button"
+          //type="button"
           onClick={this.locateMe}
           onTouchStart={this.locateMe}
-        >
+          >
           Finn Meg
         </button>
       </div>
