abc319c91feb6673a9b2e71f2506f873b892a667
Bjørnar Østtveit <bjornost@stud.ntnu.no>
2019-01-25 18:59:56 +0100

    stat

--- client/src/Komponenter/Statistikk/statistikk.js
+++ client/src/Komponenter/Statistikk/statistikk.js
@@ -1,10 +1,10 @@
 import * as React from 'react';
-import {Component} from 'react-simplified';
-import {FormGroup, FormControl} from 'react-bootstrap';
-import {statistikkService} from '../../services/statistikkService';
-import {generellServices} from '../../services/generellServices.js';
-import {feilService} from '../../services/feilService.js';
-import {GronnKnapp, StatBar} from '../../widgets.js';
+import { Component } from 'react-simplified';
+import { FormGroup, FormControl } from 'react-bootstrap';
+import { statistikkService } from '../../services/statistikkService';
+import { generellServices } from '../../services/generellServices.js';
+import { feilService } from '../../services/feilService.js';
+import { GronnKnapp, StatBar } from '../../widgets.js';
 import * as html2canvas from 'html2canvas';
 
 export class Statistikk extends Component {
@@ -18,14 +18,14 @@ export class Statistikk extends Component {
   mengde = 0;
   menyID = 0;
   menyValg = [];
-  divNavn =  "";
+  divNavn = "";
 
-  chunkArray(myArray, chunk_size){
+  chunkArray(myArray, chunk_size) {
     let index = 0;
     let arrayLength = myArray.length;
     let tempArray = [];
 
-    for(index = 0; index < arrayLength; index += chunk_size){
+    for (index = 0; index < arrayLength; index += chunk_size) {
       let myChunk = myArray.slice(index, index + chunk_size);
       tempArray.push(myChunk);
     }
@@ -33,7 +33,7 @@ export class Statistikk extends Component {
     return tempArray;
   }
 
-  async print(){
+  async print() {
 
     const filename = "Statistikk.pdf";
     let pdf = new jsPDF('p', 'mm', 'a4');
@@ -41,35 +41,35 @@ export class Statistikk extends Component {
     let runs = 0;
     let big = this.large;
 
-    function skriv(){
-      if(runs === big){
+    function skriv() {
+      if (runs === big) {
         pdf.save(filename);
       }
     }
 
-    for(let i = 0; i < this.large; i++){
+    for (let i = 0; i < this.large; i++) {
       let input = document.getElementById('barDiv' + i);
       this.teller++;
-      if(this.teller === this.total){
+      if (this.teller === this.total) {
         this.teller = 0;
       }
 
       await html2canvas(input, {
         scale: 1
       }
-        ).then(canvas => {
-          runs++;
-          pdf.addImage(canvas.toDataURL('image/png'), 'PNG', 0, 0, 200, 150);
-          if(runs != big){
-            pdf.addPage();
-          }
-    	});
+      ).then(canvas => {
+        runs++;
+        pdf.addImage(canvas.toDataURL('image/png'), 'PNG', 0, 0, 200, 150);
+        if (runs != big) {
+          pdf.addPage();
+        }
+      });
 
       skriv();
     }
   }
 
-  hide(){
+  hide() {
 
     this.maks = this.ymse[this.menyID.value].resultat.maks;
     this.large = this.ymse[this.menyID.value].resultat.large;
@@ -78,7 +78,7 @@ export class Statistikk extends Component {
     this.divNavn = streng.replace(/\s/g, "");
 
     let blokk = document.getElementById(this.divNavn);
-    if(blokk.style.display === 'none'){
+    if (blokk.style.display === 'none') {
       blokk.style.display = "block";
     } else {
       blokk.style.display = "none";
@@ -86,50 +86,39 @@ export class Statistikk extends Component {
 
   }
 
-  render(){
-    return(
-<<<<<<< HEAD
-      <>
+  render() {
+    return (
+      <React.Fragment>
         <div id="statMeny">
-          <div>
-            <GronnKnapp onClick={this.print}>Knapp</GronnKnapp>
-          </div>
-          <div>
-            <progress value={this.teller} max={this.total} ></progress>
-          </div>
-          <div>
-            <FormGroup controlId="formControlsSelect">
-              <FormControl
-                componentClass="select"
-                onChange={this.hide}
-                inputRef={(node) => {
-                  this.menyID = node;
-                }}
-              >
+          <FormGroup controlId="formControlsSelect">
+            <FormControl
+              componentClass="select"
+              onChange={this.hide}
+              inputRef={(node) => {
+                this.menyID = node;
+              }}
+            >
               {this.menyValg.map((values) => (
                 <option key={values.id} value={values.id}>
                   {values.type}
                 </option>
               ))}
-              </FormControl>
-            </FormGroup>
+            </FormControl>
+          </FormGroup>
+          <div id="lastNedBtn">
+            <GronnKnapp onClick={()=>{document.querySelector("#statMeny").style.display = "none"; setTimeout(() => {window.print(); document.querySelector("#statMeny").style.display = "inherit";}, 500)}}>Last ned som PDF</GronnKnapp>
           </div>
-=======
-      <React.Fragment>
-        <div id="lastNedBtn">
-          <GronnKnapp onClick={this.print}>Last ned som PDF</GronnKnapp>
->>>>>>> master
         </div>
-        <div id={this.divNavn} style={{display: 'block', overflowX: 'scroll', zoom: '65%', marginLeft: '15%', marginRight: '10%'}}>
-        {this.resultat.map(e => (
-          <StatBar maks={this.maks} text={this.menyValg[this.menyID.value].type} label="Antall feil" elementID={e.id} labels={e.navn} data={e.antall} ></StatBar>
-        ))}
+        <div id={this.divNavn} style={{ display: 'block', overflowX: 'scroll', zoom: '50%' }}>
+          {this.resultat.map(e => (
+            <StatBar maks={this.maks} text={this.menyValg[this.menyID.value].type} label="Antall feil" elementID={e.id} labels={e.navn} data={e.antall} ></StatBar>
+          ))}
         </div>
       </React.Fragment>
     );
   }
 
-  async mounted(){
+  async mounted() {
 
     //feil per kommune
     let info = await statistikkService.hentFeilPerKommune();
@@ -145,12 +134,12 @@ export class Statistikk extends Component {
     this.divNavn = streng.replace(/\s/g, "");
   }
 
-  splitt(info, kom, grafnavn){
-    let informasjon =  {
+  splitt(info, kom, grafnavn) {
+    let informasjon = {
       data: []
     }
 
-    for(let i = 0; i < kom.data.length; i++){
+    for (let i = 0; i < kom.data.length; i++) {
       informasjon.data.push({
         "id": kom.data[i].kommune_id,
         "navn": kom.data[i].kommune_navn,
@@ -158,9 +147,9 @@ export class Statistikk extends Component {
       });
     }
 
-    for(let i = 0; i < informasjon.data.length; i++){
-      for(let j = 0; j < info.data.length; j++){
-        if(informasjon.data[i].id === info.data[j].kommune_id){
+    for (let i = 0; i < informasjon.data.length; i++) {
+      for (let j = 0; j < info.data.length; j++) {
+        if (informasjon.data[i].id === info.data[j].kommune_id) {
           informasjon.data[i].antall = info.data[j].antall;
         }
       }
@@ -171,25 +160,25 @@ export class Statistikk extends Component {
 
     let oppdeling = this.chunkArray(informasjon.data, 15);
 
-    for(let i = 0; i < informasjon.data.length; i++){
+    for (let i = 0; i < informasjon.data.length; i++) {
       navn[i] = informasjon.data[i].navn;
       tall[i] = informasjon.data[i].antall;
     }
 
     let largest = 0;
 
-    for(let i = 0; i < tall.length; i++){
-      if(tall[i] > largest){
+    for (let i = 0; i < tall.length; i++) {
+      if (tall[i] > largest) {
         largest = tall[i];
       }
     }
 
-    let navnChunk =  this.chunkArray(navn, 15);
+    let navnChunk = this.chunkArray(navn, 15);
     let tallChunk = this.chunkArray(tall, 15);
 
     let stats = [];
 
-    for(let i = this.total; i < this.total + oppdeling.length; i++){
+    for (let i = this.total; i < this.total + oppdeling.length; i++) {
       stats.push({
         "id": i,
         "navn": navnChunk[i],
@@ -199,7 +188,7 @@ export class Statistikk extends Component {
 
     this.total += oppdeling.length;
 
-    let res =  {
+    let res = {
       "maks": largest + 5,
       "large": oppdeling.length,
       "resultat": stats
