9ddcf3dc302b956be0bd23e12f6f8e36f1bcd2e0
vebjorov <vebjorv@stud.ntnu.no>
2019-01-25 13:01:50 +0100

    ordnet litt på klassestruktur

--- client/src/Komponenter/Statistikk/statistikk.js
+++ client/src/Komponenter/Statistikk/statistikk.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import {Component} from 'react-simplified';
+import {FormGroup, FormControl} from 'react-bootstrap';
 import {statistikkService} from '../../services/statistikkService';
 import {generellServices} from '../../services/generellServices.js';
 import {feilService} from '../../services/feilService.js';
@@ -9,8 +10,15 @@ import * as html2canvas from 'html2canvas';
 export class Statistikk extends Component {
 
   resultat = [];
+  teller = 0;
+  total = 0;
   maks = 0;
   large = 0;
+  ymse = [];
+  mengde = 0;
+  menyID = 0;
+  menyValg = [];
+  divNavn =  "";
 
   chunkArray(myArray, chunk_size){
     let index = 0;
@@ -41,6 +49,10 @@ export class Statistikk extends Component {
 
     for(let i = 0; i < this.large; i++){
       let input = document.getElementById('barDiv' + i);
+      this.teller++;
+      if(this.teller === this.total){
+        this.teller = 0;
+      }
 
       await html2canvas(input, {
         scale: 1
@@ -58,23 +70,53 @@ export class Statistikk extends Component {
   }
 
   hide(){
-    let x = document.getElementById('bar');
-    if(x.style.display === 'none'){
-      x.style.display = "block";
+
+    this.maks = this.ymse[this.menyID.value].resultat.maks;
+    this.large = this.ymse[this.menyID.value].resultat.large;
+    this.resultat = this.ymse[this.menyID.value].resultat.resultat;
+    let streng = this.menyValg[this.menyID.value].type;
+    this.divNavn = streng.replace(/\s/g, "");
+
+    let blokk = document.getElementById(this.divNavn);
+    if(blokk.style.display === 'none'){
+      blokk.style.display = "block";
     } else {
-      x.style.display = "none";
+      blokk.style.display = "none";
     }
+
   }
 
   render(){
     return(
       <>
-        <div id="lastNedBtn">
-          <GronnKnapp onClick={this.print}>Last ned som PDF</GronnKnapp>
+        <div id="statMeny">
+          <div>
+            <GronnKnapp onClick={this.print}>Knapp</GronnKnapp>
+          </div>
+          <div>
+            <progress value={this.teller} max={this.total} ></progress>
+          </div>
+          <div>
+            <FormGroup controlId="formControlsSelect">
+              <FormControl
+                componentClass="select"
+                onChange={this.hide}
+                inputRef={(node) => {
+                  this.menyID = node;
+                }}
+              >
+              {this.menyValg.map((values) => (
+                <option key={values.id} value={values.id}>
+                  {values.type}
+                </option>
+              ))}
+              </FormControl>
+            </FormGroup>
+          </div>
         </div>
-        <div id="bar">
+        <div id={this.divNavn} style={{display: 'block', overflowX: 'scroll', zoom: '65%', marginLeft: '15%', marginRight: '10%'}}>
         {this.resultat.map(e => (
-          <StatBar maks={this.maks} text="Feil per kommune" label="Antall feil" elementID={e.id} text="Feil per kommune" labels={e.navn} data={e.antall} ></StatBar>
+          <StatBar maks={this.maks} text={this.menyValg[this.menyID.value].type} label="Antall feil" elementID={e.id} labels={e.navn} data={e.antall} ></StatBar>
         ))}
         </div>
       </>
@@ -82,9 +124,22 @@ export class Statistikk extends Component {
   }
 
   async mounted(){
+
+    //feil per kommune
     let info = await statistikkService.hentFeilPerKommune();
     let kom = await generellServices.hentAlleKommuner();
+    //hovedtittel kan flyttes inn til splitt metoden slik at divnavn blir satt der
+    this.splitt(info, kom, 'Feil per kommune');
 
+    this.maks = this.ymse[this.menyID.value].resultat.maks;
+    this.large = this.ymse[this.menyID.value].resultat.large;
+    this.resultat = this.ymse[this.menyID.value].resultat.resultat;
+
+    let streng = "Feil per kommune";
+    this.divNavn = streng.replace(/\s/g, "");
+  }
+
+  splitt(info, kom, grafnavn){
     let informasjon =  {
       data: []
     }
@@ -123,15 +178,12 @@ export class Statistikk extends Component {
       }
     }
 
-    this.maks = largest + 5;
-
     let navnChunk =  this.chunkArray(navn, 15);
     let tallChunk = this.chunkArray(tall, 15);
 
     let stats = [];
-    this.large = oppdeling.length;
 
-    for(let i = 0; i < oppdeling.length; i++){
+    for(let i = this.total; i < this.total + oppdeling.length; i++){
       stats.push({
         "id": i,
         "navn": navnChunk[i],
@@ -139,7 +191,25 @@ export class Statistikk extends Component {
       })
     }
 
-    this.resultat = stats;
+    this.total += oppdeling.length;
+
+    let res =  {
+      "maks": largest + 5,
+      "large": oppdeling.length,
+      "resultat": stats
+    };
+
+    this.ymse.push({
+      "id": this.mengde,
+      "resultat": res
+    });
+
+    this.menyValg.push({
+      "id": this.mengde,
+      "type": grafnavn
+    });
+
+    this.mengde++;
 
   }
 }
