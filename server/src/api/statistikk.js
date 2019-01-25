d97e2b8c819b6f8a2b1f820cddaf2dade7e1673f
vebjorov <vebjorv@stud.ntnu.no>
2019-01-25 12:58:11 +0100

    lagt grunnlag til grafer av forskjellig data

--- server/src/api/statistikk.js
+++ server/src/api/statistikk.js
@@ -5,8 +5,6 @@ import bodyParser from 'body-parser';
 import {pool} from '../../test/poolsetup.js';
 import StatistikkDao from '../dao/statistikkdao.js';
 
-var fs = require('fs');
-
 const statistikkDao = new StatistikkDao(pool);
 
 router.get('/api/feilperkommune', (req, res) => {
